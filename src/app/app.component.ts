47f535a40431be0a2177a871546cb2181daa5b89
Carlossnogueira <carlosnogueira372@gmail.com>
2026-06-25 19:33:09 -0300

    feat: Adiciona formulario de login e de criação de conta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
+import {CreateAccountFormComponent} from "./forms/create-account-form/create-account-form.component";
+import {LoginAccountFormComponent} from "./forms/login-account-form/login-account-form.component";
 
 @Component({
   selector: 'app-root',
---
+++ src/app/forms/create-account-form/create-account-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-account-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-account-form.component.html',
+  styleUrl: './create-account-form.component.css'
+})
+export class CreateAccountFormComponent {
+
+}

effc08392730b87b1ea4724c173b992e27b95281
Carlossnogueira <carlosnogueira372@gmail.com>
2026-06-29 17:04:35 -0300

    feat: adiciona validação de formulario no componente de criação e login de usuairo

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-account-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  private fb = inject(FormBuilder)
+
+  loginForm = this.fb.group({
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(8),
+        Validators.maxLength(50)
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(50)
+      ]
+    ]
+  })
+
+  get email() {
+    return this.loginForm.get('email')
+  }
+
+  get password() {
+    return this.loginForm.get('password')
+  }
+
+  onSubmit() {
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+  }
 
 }
--- src/app/features/auth/register-component/register.component.ts
+++ src/app/features/auth/register-component/register.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
 
 @Component({
   selector: 'app-register-component',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  private fb = inject(FormBuilder);
+
+  registerForm = this.fb.group({
+    username: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(100)
+      ]
+    ],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(8),
+        Validators.maxLength(50)
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(50)
+      ]
+    ]
+  });
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  onSubmit() {
+
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.registerForm.value);
+  }
 
 }
